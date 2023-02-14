af5a2dac134e46b3e69c42cfd5ecd632acde7df3
csbsundar <124553338+csbsundar@users.noreply.github.com>
2023-02-14 10:51:58 +0530

    Add files via upload

---
+++ day2/CreateLead.java
@@ -0,0 +1,35 @@
+package week2.day2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class CreateLead {
+	public static void main(String[] args) throws InterruptedException {
+	    ChromeDriver driver=new ChromeDriver();
+	    driver.get("http://leaftaps.com/opentaps/control/login");
+        driver.manage().window().maximize();
+ driver.findElement(By.id("username")).sendKeys("demoSalesManager");
+ driver.findElement(By.id("password")).sendKeys("crmsfa");
+ driver.findElement(By.className("decorativeSubmit")).click();
+ driver.findElement(By.linkText("CRM/SFA")).click();
+ driver.findElement(By.linkText("Leads")).click();
+ driver.findElement(By.linkText("Create Lead")).click();
+ driver.findElement(By.id("createLeadForm_companyName")).sendKeys("ACCENTURE");
+ driver.findElement(By.id("createLeadForm_firstName")).sendKeys("sundar");
+ driver.findElement(By.id("createLeadForm_lastName")).sendKeys("c");
+ driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("sundar");
+ driver.findElement(By.name("departmentName")).sendKeys("testing");
+ driver.findElement(By.id("createLeadForm_description")).sendKeys("Trainee");
+ driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("csb@gmail.com");
+ WebElement state = driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+	Select d1= new Select(state);
+	d1.selectByVisibleText("New York");
+	driver.findElement(By.className("smallSubmit")).click();
+	System.out.println("the title is : "+driver.getTitle());
+	Thread.sleep(3000);
+	}
+
+}
+		
---
+++ day2/training.java
@@ -0,0 +1,20 @@
+package week2.day2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class training {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ChromeDriver driver = new ChromeDriver();
+		
+		   driver.get("http://leaftaps.com/opentaps");
+		  driver.manage().window().maximize();
+		  driver.findElement(By.xpath("//input[@id='username']")).sendKeys("demosalesManager");
+		  driver.findElement(By.xpath("//input[@id='password']")).sendKeys("crmsfa");
+		  driver.findElement(By.xpath("//input[@class='decorativeSubmit']")).click();
+		 driver.findElement(By.xpath("//a[contains(text(),'CRM)]")).click();
+		 driver.findElement(By.xpath("//a[text(),'Leads')")).click();
+}
+} 
